f6cc21578b9b9b362b3d7ab5ab2d68c99d6ff786
tetramputechture <tetramputechture@gmail.com>
2021-11-15 14:31:07 -0700

    Properly store and refresh knob layout

--- omniapp/screens/knobValScreen/knobValScreen.py
+++ omniapp/screens/knobValScreen/knobValScreen.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, ObjectProperty
 from . components.omniSlider.omniSlider import OmniSlider
 from . components.slideButton.slideButton import SlideButton
 from omniapp.components.layouts.controlGroup.controlGroup import ControlGroup
@@ -13,9 +13,17 @@ from omniapp.components.layouts.controlGroup.controlGroup import ControlGroup
 
 class KnobValScreen(Screen):
     sliders = ListProperty()
+    page_layout = ObjectProperty()
 
     def on_pre_enter(self):
-        page_layout = BoxLayout(
+        # remove previous knob layout from screen
+        self.remove_widget(self.page_layout)
+ 
+        # erase previous sliders
+        self.sliders = []
+
+        # create new page layout to be filled with knobs
+        self.page_layout = BoxLayout(
             size_hint_y=0.75, pos_hint={'x': 0, 'y': 0.25}, orientation='horizontal')
 
         # fetch our current patch params so we build the right knobs
@@ -27,6 +35,7 @@ class KnobValScreen(Screen):
 
         # Screen is a group of ControlGroups, which are groups of controls (aka knobs)
         # so lets add our knobs from the current patch params we got earlier
+
         for knob_name in current_patch_params:
             slider = OmniSlider(knob_name=knob_name)
             button = SlideButton(text=knob_name)
@@ -36,6 +45,7 @@ class KnobValScreen(Screen):
             page_layout.add_widget(control_group)
             self.sliders.append(slider)
 
+        # add the knob layout to the screen
         self.add_widget(page_layout)
 
         self.manager.omni_instance.firstTime = False
