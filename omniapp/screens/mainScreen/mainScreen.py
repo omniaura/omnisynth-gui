ee2ec3e757a7ecc2d2f877fdd9a190741fd2c3ee
tetramputechture <tetramputechture@gmail.com>
2021-11-15 20:14:08 -0700

    Add open source loading gif and compile patches on boot

--- omniapp/screens/mainScreen/mainScreen.py
+++ omniapp/screens/mainScreen/mainScreen.py
@@ -6,6 +6,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.screenmanager import Screen
 from kivy.properties import BooleanProperty
 from kivy.lang import Builder
+from kivy.uix.widget import WidgetException
 import os
 import time
 
@@ -13,80 +14,60 @@ import time
 
 
 class MainScreen(Screen):
-    first_time = BooleanProperty(True)
-
     def on_pre_enter(self):
-        self.manager.omni_instance.numPatch = len(self.manager.patch_list)
+        omni = self.manager.omni_instance
+        omni.numPatch = len(self.manager.patch_list)
         patch_select_list_layout = self.ids['patch_select_list_layout']
-        if self.first_time:
-            self.manager.omni_instance.patchIndex = 0
-            if self.manager.omni_instance.numPatch > 1:
-                self.manager.slots[1].text = str(
-                    self.manager.patch_matrix[0][0])
-                self.manager.slots[2].text = str(
-                    self.manager.patch_matrix[0][1])
-            else:
-                if self.manager.omni_instance.numPatch == 1:
-                    self.manager.slots[1].text = str(
-                        self.manager.patch_matrix[0][0])
+
+        # make sure patch index is initialized, default to 0
+        omni.patchIndex = omni.patchIndex or 0
+
+        # call omni#synth_sel on our current patch
+        self.__omni_select_patch()
+
+        # set slot text
+        self.__set_slot_text()
+
+        # add slots
+        # handle exception if slots have already been added
+        # and bail out with a noop
+        try:
             patch_select_list_layout.add_widget(self.manager.slots[0])
             patch_select_list_layout.add_widget(self.manager.slots[1])
             patch_select_list_layout.add_widget(self.manager.slots[2])
-            self.first_time = False
-            self.manager.omni_instance.synth_sel(
-                self.manager.slots[1].text, OMNISYNTH_PATH)
-        else:
-            if self.manager.omni_instance.patchIndex == 0:
-                self.manager.slots[0].text = ''
-                self.manager.slots[1].text = self.manager.patch_list[0]
-                self.manager.slots[2].text = self.manager.patch_list[1]
-                self.manager.omni_instance.synth_sel(
-                    self.manager.slots[1].text, OMNISYNTH_PATH)
-            else:
-                if self.manager.omni_instance.patchIndex == self.manager.omni_instance.numPatch-1:
-                    self.manager.slots[0].text = self.manager.patch_list[self.manager.omni_instance.patchIndex - 1]
-                    self.manager.slots[1].text = self.manager.patch_list[self.manager.omni_instance.patchIndex]
-                    self.manager.slots[2].text = ''
-                    self.manager.omni_instance.synth_sel(
-                        self.manager.slots[1].text, OMNISYNTH_PATH)
-                else:
-                    self.manager.slots[0].text = self.manager.patch_list[self.manager.omni_instance.patchIndex - 1]
-                    self.manager.slots[1].text = self.manager.patch_list[self.manager.omni_instance.patchIndex]
-                    self.manager.slots[2].text = self.manager.patch_list[self.manager.omni_instance.patchIndex + 1]
-                    self.manager.omni_instance.synth_sel(
-                        self.manager.slots[1].text, OMNISYNTH_PATH)
+        except WidgetException:
+            pass
 
     def handle_up_button_release(self):
-        if self.manager.omni_instance.patchIndex != 0:
-            if self.manager.omni_instance.patchIndex == 1:
-                self.manager.slots[0].text = ''
-                self.manager.slots[1].text = str(self.manager.patch_list[0])
-                self.manager.slots[2].text = str(self.manager.patch_list[1])
-                self.manager.omni_instance.patchIndex = 0
-                self.manager.omni_instance.synth_sel(
-                    self.manager.slots[1].text, OMNISYNTH_PATH)
-            else:
-                self.manager.slots[2].text = str(
-                    self.manager.patch_list[self.manager.omni_instance.patchIndex])
-                self.manager.omni_instance.patchIndex -= 1
-                self.manager.slots[1].text = str(
-                    self.manager.patch_list[self.manager.omni_instance.patchIndex])
-                self.manager.slots[0].text = str(
-                    self.manager.patch_list[self.manager.omni_instance.patchIndex - 1])
-                self.manager.omni_instance.synth_sel(
-                    self.manager.slots[1].text, OMNISYNTH_PATH)
+        if self.manager.omni_instance.patchIndex == 0:
+            return
+
+        self.manager.omni_instance.patchIndex -= 1
+        self.__omni_select_patch()
+        self.__set_slot_text()
 
     def handle_down_button_release(self):
-        if self.manager.omni_instance.patchIndex != (self.manager.omni_instance.numPatch-1):
-            self.manager.slots[0].text = str(
-                self.manager.patch_list[self.manager.omni_instance.patchIndex])
-            self.manager.omni_instance.patchIndex += 1
-            self.manager.slots[1].text = str(
-                self.manager.patch_list[self.manager.omni_instance.patchIndex])
-            if self.manager.omni_instance.patchIndex+1 != self.manager.omni_instance.numPatch:
-                self.manager.slots[2].text = str(
-                    self.manager.patch_list[self.manager.omni_instance.patchIndex + 1])
-            else:
-                self.manager.slots[2].text = ''
-            self.manager.omni_instance.synth_sel(
-                self.manager.slots[1].text, OMNISYNTH_PATH)
+        if self.manager.omni_instance.patchIndex == (self.manager.omni_instance.numPatch-1):
+            return
+
+        self.manager.omni_instance.patchIndex += 1
+        self.__omni_select_patch()
+        self.__set_slot_text()
+
+    def __get_slot_text(self, patch_index):
+        if patch_index >= 0 and patch_index < self.manager.omni_instance.numPatch:
+            return str(self.manager.patch_list[patch_index])
+        else:
+            return ''
+
+    def __set_slot_text(self):
+        self.manager.slots[0].text = self.__get_slot_text(
+            self.manager.omni_instance.patchIndex - 1)
+        self.manager.slots[1].text = self.__get_slot_text(
+            self.manager.omni_instance.patchIndex)
+        self.manager.slots[2].text = self.__get_slot_text(
+            self.manager.omni_instance.patchIndex + 1)
+
+    def __omni_select_patch(self):
+        self.manager.omni_instance.synth_sel(
+            self.manager.patch_list[self.manager.omni_instance.patchIndex], OMNISYNTH_PATH)
