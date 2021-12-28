7515ac856e66aa472b27a58afac8a1a684d72d32
Omar Barazanji <ozanji345@gmail.com>
2021-12-28 01:01:55 -0600

    autometed whl building and post install commands

--- project-setup.py
+++ project-setup.py
@@ -1,3 +1,12 @@
 import os
 
+# create omnisynth whl if not already there
+omnisynth_dist_loc = '../omnisynth-dev/omnisynth/'
+if not os.path.isdir(omnisynth_dist_loc + 'dist/'):
+    os.system('cd ../omnisynth-dev/omnisynth/ && python -m build')
+
+# install all deps
 os.system("pip install -r requirements.txt")
+
+# post install commands
+os.system('garden install graph && garden install matplotlib')
