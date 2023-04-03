2e8cb8928aa213d000aa97d09bf66deda0f4ee09
Omar Barazanji <ozanji345@gmail.com>
2023-04-03 17:31:29 -0500

    updated

--- project-setup.py
+++ project-setup.py
@@ -1,9 +1,8 @@
 import os
 
 # create omnisynth whl if not already there
-omnisynth_dist_loc = '../omnisynth-dev/omnisynth/'
-if not os.path.isdir(omnisynth_dist_loc + 'dist/'):
-    os.system('cd ../omnisynth-dev/omnisynth/ && python -m build')
+# omnisynth_dist_loc = '../omnisynth-dev/omnisynth/'
+os.system('cd ../omnisynth-dev/omnisynth/ && python -m build')
 
 # install all deps
 os.system("pip install -r requirements.txt")
